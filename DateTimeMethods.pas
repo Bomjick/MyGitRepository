12490e518635ab6f3d29a4a4537b0ff3b7d76af8
Senichev <senichevleonid@yandex.ru>
2019-11-25 17:10:26 +0300

    addTimeMethods

--- DateTimeMethods.pas
+++ DateTimeMethods.pas
@@ -4,6 +4,20 @@ begin
 result:=year mod 4=0;
 end;
 function DaysInMonth(month, day: integer): integer := 0;
+if IsLeapYear=true then
+  case month of
+    1,3,5,7,8,10,12: days:=31;
+    4,6,9,11: days:=30;
+    2: days:=29;
+    
+  end
+  else
+  case month of
+    1,3,5,7,8,10,12: days:=31;
+    4,6,9,11: days:=30;
+    2: days:=28;
+  end;
+  writeln(days);
 
 function LaterInDay(p1, p2: DateTime): DateTime := p1;
 function LaterInYear(p1, p2: DateTime): DateTime := p1;
