ec280dfaafaa46adc515139fc7d949cea05bdbc2
Bomjick <senichevleonid@yandex.ru>
2019-12-05 16:13:13 +0300

    Revert "add SecondsInHours + fix errors"
    This reverts commit 9c332901efc68a7d3c73cbf12d9175e9daa26681.

--- DateTimeMethods.pas
+++ DateTimeMethods.pas
@@ -1,52 +1,53 @@
 ﻿unit DateTimeMethods;
-
 function IsLeapYear(year: integer): boolean;
 begin
-  result := year mod 4 = 0;
+result:=year mod 4=0;
 end;
-
-function DaysInMonth(month, year: integer): integer;
+function DaysInMonth(month, day: integer): integer;
 begin
+if IsLeapYear=true then
   case month of
-    1, 3, 5, 7, 8, 10, 12: result := 31;
-    4, 6, 9, 11: result := 30;
-    2:
-      begin
-        if IsLeapYear(year) then
-          result := 29
-        else result := 28;
-      end; 
-  
+    1,3,5,7,8,10,12: days:=31;
+    4,6,9,11: days:=30;
+    2: days:=29;
+    
+  end
+  else
+  case month of
+    1,3,5,7,8,10,12: days:=31;
+    4,6,9,11: days:=30;
+    2: days:=28;
   end;
-end;
+  writeln(days);
 
 function LaterInDay(p1, p2: DateTime): DateTime := p1;
 
 
-function LaterInYear(p1, p2: DateTime): DateTime := (p1.Date > p2.Date) ? p1 : p2;
-
-
-
-
-function DaysInYear(year: integer): integer := (IsLeapYear(year)) ? 366 : 365;
-
-
-function DaysInYearRange(year1, year2: integer): integer;
+function LaterInYear(p1, p2: DateTime): DateTime := p1;
 begin
-  var a, b: integer;
-  a := (IsLeapYear(year1)) ? 366 : 365;
-  b := (IsLeapYear(year2)) ? 366 : 365;
-  result := a + b
+  result := (p1.Date > p2.Date)? p1:p2;
   
 end;
 
-function SecondsInHours(hours: integer): integer;
-begin
-  Assert(hours > 0);
-  readln(hours);
-  writeln(hours * 3600);
+    
+  
+  
 end;
 
+function DaysInYear(year: integer): integer := 0;
+var a: integer;
+  result:=a:=(IsLeapYear(year)))?366:365;
+
+function DaysInYearRange(year1, year2: integer): integer);
+begin
+  var a,b: integer;
+  a:=(IsLeapYear(year)))?366:365;
+  b:=(IsLeapYear(year)))?366:365;
+  result:=a+b
+ 
+end;
+function SecondsInHours(hours: integer): integer := 0;
+
 begin
 
-end. 
+end.
